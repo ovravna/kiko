fbf10af5a22b3ce299cd77c945bda979bbe17320
Ole Ravna <olevra@stud.ntnu.no>
2019-11-21 21:21:39 +0100

    Add termios

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::io::{stdin, Read};
+use termios::*;
+
+const STDIN_FD: i32 = 0;
+
+struct RawMode {
+    termios: Termios
+}
+impl Drop for RawMode {
+    fn drop(&mut self) {
+        disable_raw_mode(self.termios);
+    }
+}
+
+fn run() {
+
+   let mut res = [0];
+   while let Ok(_) = stdin().read(&mut res) {
+
+       if res[0] == 'q' as u8{
+           break;
+       }
+
+       println!("{}", res[0]);
+   }
+}
+
+fn disable_raw_mode(termios: Termios) {
+    tcsetattr(STDIN_FD, TCSAFLUSH, &termios).unwrap();
+}
+fn enable_raw_mode() -> RawMode {
+
+    let mut raw = Termios::from_fd(STDIN_FD).unwrap();
+
+    tcgetattr(STDIN_FD, &mut raw).unwrap();
+    raw.c_lflag &= !ECHO;
+
+    tcsetattr(STDIN_FD, TCSAFLUSH, &mut raw).unwrap();
+
+    return RawMode { termios: raw }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _rm = enable_raw_mode(); 
+    run()
 }
